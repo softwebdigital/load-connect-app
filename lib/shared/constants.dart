d33c715c7737bdc8d9f160829b84b7ed77e2df77
toykam <kabirtoyyib19@gmail.com>
2022-10-24 13:00:11 +0100

    added

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -3,3 +3,11 @@ class AppData {
   static const googleMapsApiKey = "AIzaSyCGD0gE33dc-6UHtIAptXSAVZRogFvV8Hs";
   static const apiUrl = "http://137.184.146.138/api/v1";
 }
+
+
+const userDataBox = 'user-data-box';
+const userIdKey = 'user-id-key';
+const userTokenKey = 'user-token-key';
+
+const appDataBox = 'app-data-box';
+const loggedInBeforeKey = 'logged-in-before-key';
